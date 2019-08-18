1ad34a138f1d6f98781bd952caae8d05f979d2b2
Thierry Godfroid <thierry.godfroid@gmail.com>
2019-08-18 11:36:45 +0200

    add basic zsh support

---
+++ zsh-custom/tgo.zsh
@@ -0,0 +1,33 @@
+builtin echo "CUSTOM ZSH CONFIG"
+# nvm
+export NVM_DIR="/Users/tgo/Documents/scripts/nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
+
+# ------------------------------------------------- Generic aliases and helpers
+# set all aliases for linux based systems.  Mac OS specific section below
+# will overrided when needed
+
+alias ll='ls -laF --color=auto'
+alias l='ll'
+alias less='less -F' # auto exit if one screen or less
+alias cgrep='grep -iHn --color=auto'
+alias cegrep='egrep --color=auto'
+manf() { man $1 | less -p "^ +$2"; } # open manpage and jump to option
+randpwd() { tr -dc "[:alnum:]" < /dev/urandom | head -c ${1:-8}; echo; }
+spot() { locate "$@"; }
+
+# ------------------------------ Mac OS Specific rewrite of aliases and helpers
+if [[ "$OSTYPE" == "darwin"* ]]; then
+
+  alias ll='ls -laFG'
+  alias gvim='mvim +NERDTree'
+  alias cleanupDS="find . -type f -name '*.DS_Store' -ls -delete"
+  alias ql='qlmanage -p 2>/dev/null' # quicklook a file with ql <file>
+  spot() { mdfind "kMDItemDisplayName == '$@'wc"; }
+  randpwd() {
+    LC_ALL=C tr -dc 'A-Za-z0-9_!@#$%&()\-+=' < /dev/urandom | head -c ${1:-8};
+    echo;
+  }
+  # toggle viewing hidden files in finder
+  alias hidden='CUR=$( defaults read com.apple.finder AppleShowAllFiles ); SETTO="NO"; [ "$CUR" == "NO" ] && SETTO="YES"; defaults write com.apple.finder AppleShowAllFiles $SETTO; killall Finder /System/Library/CoreServices/Finder.app;'
+fi
